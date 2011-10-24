56f17dfc2d5e883d31c4fea9f4c16d230208b6e4
Casey Beach <beachc@gmail.com>
2011-10-23 23:40:58 -0700

    I just uploaded my first bot.  Currently ranked 1800 something.  Next step will be to implement A* or better yet D* if I can find a good explanation of it somewhere.

--- googleAIChallenge/tutorial.sh
+++ googleAIChallenge/tutorial.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python tools/playgame.py "python MyBot.py" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 --verbose -e
+python tools/playgame.py "python SpockBot.py" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 --verbose -e
